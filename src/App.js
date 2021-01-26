28e1b166012adf5a2e4e90e54a43cc71c67d77dc
gbazhina <gbazhina@ya.ru>
2021-01-26 10:53:55 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello world! React! New
+          Hello world! My patch1
         </p>
       </header>
     </div>
